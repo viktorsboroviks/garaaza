a9f5dc895fa17aeb5c584745a637c784b5f5074a
Viktors Boroviks <vboroviks@inbox.lv>
2024-08-01 14:27:51 +0300

    Reserved i values removed

--- include/garaza.hpp
+++ include/garaza.hpp
@@ -13,48 +13,23 @@ bool vector_contains(const std::vector<T>& v, const T& x)
     return std::find(v.begin(), v.end(), x) != v.end();
 }
 
-static const size_t I_FIRST_AVAILABLE = SIZE_MAX;
-static const size_t I_RANDOM = I_FIRST_AVAILABLE - 1;
-
 template <typename T>
 class Storage {
 public:
-    size_t add(T x, size_t i = I_FIRST_AVAILABLE)
+    size_t add(T x)
     {
-        if (i == I_FIRST_AVAILABLE) {
-            return _add_at_first_available_i(x);
-        }
+        return _add_at_first_available_i(x);
+    }
 
-        if (i == I_RANDOM) {
-            return _add_at_random_i(x);
-        }
-
-        // expand storage if needed
-        if (i >= _storage.size()) {
-            const size_t new_storage_size = i + 1;
-            _storage.resize(new_storage_size);
-            for (size_t fi = _storage.size(); fi < new_storage_size; fi++) {
-                _free_i.insert(fi);
-            }
-        }
-
+    void replace(T x, size_t i)
+    {
+        assert(contains_i(i));
         _storage[i] = x;
-        _free_i.erase(i);
-        return i;
     }
 
     size_t remove(size_t i)
     {
-        if (i == I_FIRST_AVAILABLE) {
-            return remove(all_i()[0]);
-        }
-
-        if (i == I_RANDOM) {
-            return remove(rnd_i());
-        }
-
-        assert(i < _storage.size());
-        assert(!_free_i.contains(i));
+        assert(contains_i(i));
         _free_i.insert(i);
         assert(_free_i.size() <= _storage.size());
         return i;
@@ -62,8 +37,9 @@ public:
 
     T* at(size_t i)
     {
-        if (i == I_FIRST_AVAILABLE || i == I_RANDOM || i >= _storage.size() ||
-            _free_i.contains(i)) {
+        assert(i < _storage.size());
+        if (_free_i.contains(i)) {
+            assert(!contains_i(i));
             return nullptr;
         }
 
@@ -111,9 +87,9 @@ public:
         return all_i().size();
     }
 
-    size_t max_size()
+    bool empty()
     {
-        return I_RANDOM - 1;
+        return all_i().size() == 0;
     }
 
     bool contains(T x)
@@ -144,25 +120,6 @@ private:
         // first try to occupy free storage
         if (!_free_i.empty()) {
             size_t i = *_free_i.begin();
-            _storage[i] = x;
-            _free_i.erase(i);
-            return i;
-        }
-
-        // if no free storage available, extend storage
-        _storage.push_back(x);
-        return _storage.size() - 1;
-    }
-
-    size_t _add_at_random_i(T x)
-    {
-        assert(_free_i.size() <= _storage.size());
-
-        // first try to occupy free storage
-        if (!_free_i.empty()) {
-            const std::vector<size_t> v_free_i(_free_i.begin(), _free_i.end());
-            const size_t i_i = rododendrs::rnd01() * v_free_i.size();
-            const size_t i = v_free_i[i_i];
             _storage[i] = x;
             _free_i.erase(i);
             return i;
